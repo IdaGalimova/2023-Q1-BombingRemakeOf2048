d819107784525f95d05b530b41d637655950d18e
skailiut <skailiut@gmail.com>
2023-10-25 22:27:01 +0200

    Added bombing front-end

--- Pages/PlayPage.java
+++ Pages/PlayPage.java
@@ -9,11 +9,13 @@ public class PlayPage  {
     GameSetup gameSetup = new GameSetup();
 
     Color darkBlue = new Color(0, 48, 73);
+    Color darkBluelighter = new Color(58, 94, 122);
     Color sandy = new Color(234, 226, 183);
     Color yellow = new Color(252, 191, 73);
     Color red = new Color(214, 40, 40);
     Color orange = new Color(247, 127, 0);
     Font moonspaced = new Font("Monospaced", Font.ITALIC | Font.BOLD, 30);
+    Font sidePanelTextFont = new Font("Monospaced", Font.ITALIC | Font.BOLD, 20);
     Font scroreFont = new Font("Monospaced", Font.ITALIC | Font.BOLD, 15);
 
     JButton[][] buttons;
@@ -40,7 +42,7 @@ public class PlayPage  {
         JFrame playFrame = new JFrame();
         playFrame.setTitle("PLAYING GAME");
         playFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        playFrame.setSize(600, 600);
+        playFrame.setSize(800, 600);
         playFrame.setLayout(null);
         playFrame.getContentPane().setBackground(darkBlue);
         playFrame.setVisible(true);
@@ -55,7 +57,7 @@ public class PlayPage  {
         // Setting up back button:
         JButton backButton = new JButton("Go Back");
         backButton.setBounds(0, 0, 100, 20);
-        backButton.setBackground(yellow);
+        backButton.setBackground(darkBluelighter);
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -68,8 +70,8 @@ public class PlayPage  {
         // Setting up panel to show the SuperTile abilities
 
         JPanel sidePanel = new JPanel();
-        sidePanel.setBounds(520, 80, 60, 400); 
-        sidePanel.setBackground(Color.BLUE); 
+        sidePanel.setBounds(520, 100, 200, 400); 
+        sidePanel.setBackground(darkBluelighter); 
         sidePanel.setVisible(false); 
         playFrame.add(sidePanel);
 
@@ -78,6 +80,8 @@ public class PlayPage  {
         JButton closeButton = new JButton("Close");
         sidePanel.add(closeButton);
         closeButton.setBounds(10, 10, 40, 20);
+
+        // Setting up the panel for the buttons
 
         JPanel panel = new JPanel(new GridLayout(4, 4));
         panel.setBounds(100, 100, 400, 400);
@@ -109,37 +113,73 @@ public class PlayPage  {
         // Adding action listeners and bombing features
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++){
+                int pressedButtonI = i;
+                int pressedButtonJ = j;
+
                 buttons[i][j].addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        
+
+                        JLabel valueOftile = new JLabel(gameSetup.getGrid()[pressedButtonI][pressedButtonJ].getValue() + ":");
+                        //valueOftile.setBounds(570, 250, 70, 50);
+                        valueOftile.setFont(sidePanelTextFont);
+                        sidePanel.add(valueOftile);
+
+                        JLabel bombingsLeftText = new JLabel("Bombings left:");
+                        //bombingsLeftText.setBounds(570, 300, 70, 50);
+                        bombingsLeftText.setFont(sidePanelTextFont);
+                        sidePanel.add(bombingsLeftText);
+
+                        JLabel bombingsLeft = new JLabel("3");
+                        //bombingsLeft.setBounds(570, 350, 70, 50);
+                        bombingsLeft.setFont(sidePanelTextFont);
+                        sidePanel.add(bombingsLeft);
+
+
                         sidePanel.setVisible(true); // Show the side panel when a button is clicked
-
+                        playFrame.setSize(800, 600);     
                         
-                        
-                        // Adding borders to the tiles that can be bombed
-
+                        // Adding borders to the tiles that can be bombed, disabling all
+                        // buttons that cannot be bombed 
                         for (int m = 0; m < 4; m++) {
                             for (int n = 0; n < 4; n++) {
-                                if(gameSetup.getGrid()[m][n].getValue() == 2) {
-                                buttons[m][n].setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
+
+                                buttons [m][n].setEnabled(false);
+                                if (m == pressedButtonI && n == pressedButtonJ){
+                                    buttons[m][n].setEnabled(true);
                                 }
+
+                                if (gameSetup.getGrid()[m][n].getValue() == 2) {
+                                    buttons[m][n].setBorder(BorderFactory.createLineBorder(Color.RED, 4));
+                                    buttons[m][n].setEnabled(true);
+                                } 
                             }
                         }
-
+                        
                         // Close button 
                         closeButton.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
+                                sidePanel.remove(valueOftile);
+                                sidePanel.remove(bombingsLeftText); 
+                                sidePanel.remove(bombingsLeft);                                
                                 sidePanel.setVisible(false);
-                                playFrame.remove(closeButton); 
+                                playFrame.remove(closeButton);
+
+                                // Unchanging the boarders
 
                                 for (int m = 0; m < 4; m++) {
                                     for (int n = 0; n < 4; n++) {
-                                        if(gameSetup.getGrid()[m][n].getValue() == 2) {
-                                            buttons[m][n].setBorder(BorderFactory.createLineBorder(darkBlue, 2));
+                                        buttons[m][n].setEnabled(true);
+                                        
+                                            if (gameSetup.getGrid()[m][n].getValue() == 2) {
+                                                buttons[m][n].setBorder(BorderFactory.createLineBorder(darkBlue, 2));
+                                        }
+                                    }
                                 }
-                            }
-                        }
+
+                                playFrame.setSize(800, 600);
 
                             }
                         });
@@ -215,6 +255,7 @@ public class PlayPage  {
                 redrawGrid();
 
                 if (gameSetup.checkGameOver()) {
+                    
                     gameOverPage.runGameOver();
                     redrawGrid();
                 }
